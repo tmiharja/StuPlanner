006ab7d88645a995966da25074e7f23d940732ea
Toni Miharja <168tonimiharja@gmail.com>
2017-03-22 16:41:04 +0800

    Added methods to StudentManager and other logics

--- src/planner/manager/StudentMgr.java
+++ src/planner/manager/StudentMgr.java
@@ -1,5 +1,34 @@
 package planner.manager;
+import java.util.ArrayList;
+import java.util.Scanner;
+import planner.db.PlannerData;
+import planner.model.Student;
+
+/**
+ * Student Manager Class manages the creation, removal 
+ * of students from the student list
+ * @version 1.0
+ * @since 2017-03-21
+ */
 
 public class StudentMgr {
+	
+	private static ArrayList<Student> students = PlannerData.students;
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static ArrayList<Student> getStudents(){ return students; }
+	
+	
+	/**
+	 * Create a new student having the all necessary information.
+	 * @param matricNumber
+	 * @param gender
+	 * @param nationality
+	 */	
+	public static void Student (String matricNumber, char gender, String nationality) {
+		Student newStudent = new Student(matricNumber, gender, nationality);
+		students.add((Student) newStudent);
+		System.out.println("Student of matric no " + matricNumber + " was added to the food menu.");
+	}
 
 }
