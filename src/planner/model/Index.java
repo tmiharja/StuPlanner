94c7bd581f10e9f5ed0aee43d6288df56f8282c9
Toni Miharja <168tonimiharja@gmail.com>
2017-03-23 20:44:47 +0800

    added new attributes

--- src/planner/model/Index.java
+++ src/planner/model/Index.java
@@ -6,7 +6,45 @@ public class Index {
 	
 	private int vacancy;
 	
+	private int isLecture;
+
+	private int isTutorial;
+	
+	private int isLab;
+	
 	Lesson lessonSchedule;
+	
+	public Index (int indexNumber, int vacancy, int isLecture, int isTutorial, int isLab){
+		this.indexNumber = indexNumber;
+		this.vacancy = vacancy;
+		this.isLecture = isLecture;
+		this.isTutorial = isTutorial;
+		this.isLab = isLab;
+	}
+	
+	public int isLecture() {
+		return isLecture;
+	}
+
+	public void setLecture(int isLecture) {
+		this.isLecture = isLecture;
+	}
+
+	public int isTutorial() {
+		return isTutorial;
+	}
+
+	public void setTutorial(int isTutorial) {
+		this.isTutorial = isTutorial;
+	}
+
+	public Lesson getLessonSchedule() {
+		return lessonSchedule;
+	}
+
+	public void setLessonSchedule(Lesson lessonSchedule) {
+		this.lessonSchedule = lessonSchedule;
+	}
 
 	public int getIndexNumber() {
 		return indexNumber;
@@ -22,6 +60,14 @@ public class Index {
 
 	public void setVacancy(int vacancy) {
 		this.vacancy = vacancy;
+	}
+
+	public int isLab() {
+		return isLab;
+	}
+
+	public void setLab(int isLab) {
+		this.isLab = isLab;
 	}
 
 }
