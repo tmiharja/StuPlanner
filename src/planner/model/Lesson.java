df38ed4da7807d9f974a743424d0105b1d5d504d
Toni Miharja <168tonimiharja@gmail.com>
2017-03-23 12:11:08 +0800

    Lesson code

--- src/planner/model/Lesson.java
+++ src/planner/model/Lesson.java
@@ -1,5 +1,30 @@
 package planner.model;
 
+import java.util.Calendar;
+
 public class Lesson {
+	/**
+	 * The venue of the lesson
+	 */
+	private String venue;
+	
+	/**
+	 * The time of the lesson
+	 */
+	private Calendar lessonTime;
+	
+	/**
+	 * Boolean attribute to indicate whether or not
+	 * the index has tutorial/ lecture/ lab
+	 */
+	private boolean hasClass;
+
+	public Calendar getLessonTime() {
+		return lessonTime;
+	}
+
+	public void setLessonTime(Calendar lessonTime) {
+		this.lessonTime = lessonTime;
+	}
 
 }
