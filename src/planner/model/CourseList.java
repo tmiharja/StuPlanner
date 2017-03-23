7f5d324d4e8f4c73c2c682164d46d314b8bf2b07
Toni Miharja <168tonimiharja@gmail.com>
2017-03-23 18:38:14 +0800

    added courseList specification

--- src/planner/model/CourseList.java
+++ src/planner/model/CourseList.java
@@ -1,5 +1,20 @@
 package planner.model;
 
+import java.util.ArrayList;
+
+import planner.db.CoursesData;
+
 public class CourseList {
+	
+	public static ArrayList<Course> CourseList = CoursesData.CourseList;
+
+	public static ArrayList<Course> getCourseList() {
+		return CourseList;
+	}
+
+	public static void setCourseList(ArrayList<Course> courseList) {
+		CourseList = courseList;
+	}
+	
 
 }
