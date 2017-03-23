ab75e3e2c0ee08a860452f55aecf3cc30f94091f
Toni Miharja <168tonimiharja@gmail.com>
2017-03-23 20:47:59 +0800

    added methods to IndexMgr

--- src/planner/manager/IndexMgr.java
+++ src/planner/manager/IndexMgr.java
@@ -1,5 +1,23 @@
 package planner.manager;
 
+import planner.model.Course;
+import planner.model.Index;
+
+
 public class IndexMgr {
+	
+	/**
+	 * Create a new index specific to the course
+
+	 */	
+	public static Index addIndex (Course course, int indexNumber, int vacancy, int isLecture, int isTutorial, int isLab)  {
+		
+		Index newIndex = new Index(indexNumber, vacancy, isLecture, isTutorial, isLab);
+		
+		(course.getIndexList()).add((Index) newIndex);
+		
+		System.out.println("Index Number " + indexNumber + " of course" + course.getCourseName() + " has been added!");
+		return newIndex;
+	}
 
 }
